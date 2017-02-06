62b52a90eaccfb671a648edb8bd2f79171036bc6
Milos Stojanovic <stojanovicmilos92@outlook.com>
2017-02-06 20:57:49 +0100

    Implemented flow

--- src/flow.js
+++ src/flow.js
@@ -1,19 +1,79 @@
-export function id(obj) {
-    return obj;
+/**
+ * Module implementing functional combinators for flow of control.
+ */
+
+/**
+ * Identity combinator which creates a function that returns the item 
+ * passed as a parameter.
+ * @param Identity item.
+ * @return Function returning an item given as a parameter.
+ */
+export function id(item) {
+    return function () {
+        return item;
+    };
 }
 
-export function tap() {
-
+/**
+ * K-combinator used to bridge void functions.
+ * @param Target function that needs to be bridged.
+ * @return Function which applies its args to target function and
+ * returns them.
+ */
+export function tap(func) {
+    return function (...args) {
+        if (typeof func === 'function')
+            func.apply(this, args);
+        if (args.length === 1)
+            return args[0];
+        return args;
+    };
 }
 
-export function alt() {
-
+/**
+ * OR-combinator used for simple conditional logic.
+ * @param func1 which return value is returned if it evaluates to true
+ * @param func2 which return value is returned if func1 doesnt evalute
+ * to true
+ * @return Return value of func1 if it evaluates to true or return value
+ * of func2
+ */
+export function alt(func1, func2) {
+    return function (arg) {
+        if (typeof func1 !== 'function' || typeof func2 !== 'function')
+            return undefined;
+        return func1(arg) || func2(arg);
+    };
 }
 
+/**
+ * S-combinator used to loop over a sequence of functions
+ * @param sequence of functions with arity of one
+ * @return Function which takes one parameter and runs the sequence
+ * of function against a given a parameter
+ */
 export function seq() {
-
+    const funcs = Array.prototype.slice.call(arguments);
+    return function (arg) {
+        funcs.forEach(function (fn) {
+            if (typeof fn == 'function') {
+                fn(arg);
+            }
+        });
+    };
 }
 
-export function fork() {
-
+/**
+ * @param join function which takes two arguments
+ * @param func1 function which takes one argument
+ * @param func2 function which takes one argument
+ * @return Function which takes one argument, applies it to
+ * func1 and func2 and calls join with results
+ */
+export function fork(join, func1, func2) {
+    return function (val) {
+        if (typeof join !== 'function')
+            return undefined;
+        return join(func1(val), func2(val));
+    };
 }
--- test/flow-test.js
+++ test/flow-test.js
@@ -1,16 +1,129 @@
 import * as Flow from "../src/flow";
 import { expect } from "chai";
+import { spy } from "sinon";
 
 describe("testing flow", () => {
-    it("creates identity lense and evaluates it", () => {
-        var obj = { i: 10, s: "abc"};
+    it("creates identity combinator and evaluates it", () => {
+        var obj = { i: 10, s: "abc" };
         var arr = ["a", "b", 1, 2];
-        expect(Flow.id(10)).equal(10);
-        expect(Flow.id(true)).equal(true);
-        expect(Flow.id("string")).equal("string");
-        expect(Flow.id(obj)).equal(obj);
-        expect(Flow.id(obj)).deep.equal(obj);
-        expect(Flow.id(arr)).equal(arr);
-        expect(Flow.id(arr)).deep.equal(arr);
+        expect(Flow.id(10)()).equal(10);
+        expect(Flow.id(true)()).equal(true);
+        expect(Flow.id("string")()).equal("string");
+        expect(Flow.id(obj)()).equal(obj);
+        expect(Flow.id(obj)()).deep.equal(obj);
+        expect(Flow.id(arr)()).equal(arr);
+        expect(Flow.id(arr)()).deep.equal(arr);
+    });
+
+    it("executes a tap combinator with valid function", () => {
+        var func1 = spy();
+        var result1 = Flow.tap(func1)("arg");
+        expect(func1.calledOnce).equal(true);
+        expect(func1.firstCall.args[0]).equal("arg");
+        expect(result1).equal("arg");
+
+        var func2 = spy();
+        var result2 = Flow.tap(func2)("arg1", "arg2", "arg3");
+        expect(func2.calledOnce).equal(true);
+        expect(func2.firstCall.args[0]).equal("arg1");
+        expect(func2.firstCall.args[1]).equal("arg2");
+        expect(func2.firstCall.args[2]).equal("arg3");
+        expect(result2).deep.equal(["arg1", "arg2", "arg3"]);
+    });
+
+    it("executes a tap combinator and skips invalid function", () => {
+        expect(Flow.tap(undefined)("arg")).equal("arg");
+        expect(Flow.tap("func")("arg")).equal("arg");
+        expect(Flow.tap({})("arg")).equal("arg");
+        expect(Flow.tap(100)("arg")).equal("arg");
+
+        expect(Flow.tap(undefined)("arg1", "arg2", "arg3"))
+            .deep.equal(["arg1", "arg2", "arg3"]);
+        expect(Flow.tap("func")("arg1", "arg2", "arg3"))
+            .deep.equal(["arg1", "arg2", "arg3"]);
+        expect(Flow.tap({})("arg1", "arg2", "arg3"))
+            .deep.equal(["arg1", "arg2", "arg3"]);
+        expect(Flow.tap(100)("arg1", "arg2", "arg3"))
+            .deep.equal(["arg1", "arg2", "arg3"]);
+    });
+
+    it("executes an alt combinator with valid functions", () => {
+        var func1 = function (num) { return false; };
+        var func2 = function (num) { return num; };
+        
+        var result = Flow.alt(func1, func2)(1);
+        expect(result).equal(1);
+        result = Flow.alt(func2, func1)(1);
+        expect(result).equal(1);
+    });
+
+    it("executes an alt combinator with invalid functions", () => {
+        var func1 = function (num) { return false; };
+        var func2 = function (num) { return num; };
+        
+        var result = Flow.alt(undefined, func2)(1);
+        expect(result).equal(undefined);
+        result = Flow.alt(func1, undefined)(1);
+        expect(result).equal(undefined);
+    });
+
+    it("executes an seq combinator with valid functions", () => {
+        var func1 = spy();
+        var func2 = spy();
+        var func3 = spy();
+
+        var arg1 = (a) => func1(a);
+        var arg2 = (a) => func2(a);
+        var arg3 = (a) => func3(a);
+
+        Flow.seq(arg1, arg2, arg3)("arg");
+        expect(func1.firstCall.args[0]).to.equal("arg");
+        expect(func2.firstCall.args[0]).to.equal("arg");
+        expect(func3.firstCall.args[0]).to.equal("arg");
+    });
+
+    it("executes an seq combinator and skips invalid functions", () => {
+        var func1 = spy();
+        var func2 = spy();
+        var func3 = spy();
+
+        var arg1 = (a) => func1(a);
+        var arg2 = (a) => func2(a);
+        var arg3 = (a) => func3(a);
+
+        Flow.seq("func1", func2, undefined)("arg");
+        expect(func1.called).to.equal(false);
+        expect(func2.firstCall.args[0]).to.equal("arg");
+        expect(func3.called).to.equal(false);
+    });
+
+    it("executes a fork combinator with valid functions", () => {
+        var func1 = spy();
+        var func2 = spy();
+        var func3 = spy();
+
+        var arg1 = (a) => func1(a);
+        var arg2 = (a) => a + 10;
+        var arg3 = (a) => a + 20;
+
+        Flow.fork(func1, arg2, arg3)(1);
+        expect(func1.called).to.equal(true);
+        expect(func1.firstCall.args[0]).to.equal(11);
+        expect(func1.firstCall.args[1]).to.equal(21);
+    });
+
+    it("executes a fork combinator with invalid function and returns 'undefined'", () => {
+        var func1 = spy();
+        var func2 = spy();
+        var func3 = spy();
+
+        var arg1 = (a) => func1(a);
+        var arg2 = (a) => a + 10;
+        var arg3 = (a) => a + 20;
+
+        Flow.fork(func1, arg2, arg3)(1);
+        expect(func1.called).to.equal(true);
+        expect(func1.firstCall.args[0]).to.equal(11);
+        expect(func1.firstCall.args[1]).to.equal(21);
     });
 });
