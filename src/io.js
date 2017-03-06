1417e63c77b980f02a97276cccb6134a811230fd
Milos Stojanovic <stojanovicmilos92@outlook.com>
2017-03-06 21:52:43 +0100

    Implementing IO monad, need to finish tests

---
+++ src/io.js
@@ -0,0 +1,39 @@
+/**
+ * @class IO
+ */
+class IO {
+
+    constructor(effect) {
+        if (effect instanceof Function) {
+            throw new Error('Function required for IO');
+        }
+        this.effect = effect;
+    }
+
+    static of(val) {
+        return new IO(() => val);
+    }
+
+    static from(func) {
+        return new IO(func);
+    }
+
+    map(func) {
+        var self = this;
+        return new IO(function () {
+            return func(self.effect());
+        });
+    }
+
+    chain(func) {
+        return fn(this.effect());
+    }
+
+    run() {
+        return this.effect();
+    }
+}
+
+export {
+    IO
+}
---
+++ test/io-test.js
@@ -0,0 +1,8 @@
+import { IO } from "../src/io";
+import { expect, assert } from "chai";
+import { spy } from "sinon";
+
+describe("testing IO monad", () => {
+
+    it("TODO");
+});
