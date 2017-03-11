2f734588251e83b96fd03bf65049cab81ff97437
Milos Stojanovic <stojanovicmilos92@outlook.com>
2017-03-11 17:46:51 +0100

    Edited io and maybe test descriptions

--- test/io-test.js
+++ test/io-test.js
@@ -4,32 +4,32 @@ import { spy } from "sinon";
 
 describe("testing IO monad", () => {
 
-    it("static method 'of' returns object of correct type", () => {
+    it("static method 'of' returns instance of 'IO' with function returning the given parameter", () => {
         var io = IO.of("value");
         expect(io instanceof IO).to.equal(true);
         expect(io.effect()).to.equal("value");
     });
 
-    it("static method 'from' return object of correct type", () => {
+    it("static method 'from' returns instance of 'IO' with a given function", () => {
         var io = IO.from(() => 100);
         expect(io instanceof IO).to.equal(true);
         expect(io.effect()).to.equal(100);
         assert.throws(() => { IO.from(100) }, Error, "Function required for IO");
     });
 
-    it("'map' method returns object of correct type", () => {
+    it("'map' method applies the given function on monad value and wrapps the result as 'IO' instance", () => {
         var io = IO.from(() => 10).map((val) => val * 5);
         expect(io instanceof IO).to.equal(true);
         expect(io.effect()).to.equal(50);
     });
 
-    it("'chain' method returns object of correct type", () => {
+    it("'chain' method applies the given function on monad value and returns the result", () => {
         var io = IO.from(() => 10).chain((val) => val * 5);
         expect(io instanceof IO).to.equal(false);
         expect(io).to.equal(50);
     });
 
-    it("'run' method returns object of correct type", () => {
+    it("'run' method runs the monad value function and return its result", () => {
         var io = IO.from(() => 10).run();
         expect(io instanceof IO).to.equal(false);
         expect(io).to.equal(10);
--- test/maybe-test.js
+++ test/maybe-test.js
@@ -4,31 +4,31 @@ import { spy } from "sinon";
 
 describe("testing Maybe monad", () => {
 
-    it("static method 'just' returns object of 'Just' type", () => {
+    it("static method 'just' returns instance of 'Just'", () => {
         var maybe = Maybe.just("value");
         expect(maybe instanceof Just).to.equal(true);
     });
 
-    it("static method 'nothing' returns object of 'Nothing' type", () => {
+    it("static method 'nothing' returns instance of 'Nothing'", () => {
         var maybe = Maybe.nothing("value");
         expect(maybe instanceof Nothing).to.equal(true);
     });
 
-    it("static method 'of' returns object of correct type", () => {
+    it("static method 'of' returns 'Just' or 'Nothing' depending on the value passed", () => {
         var maybeVal = Maybe.of("value");
         expect(maybeVal instanceof Just).to.equal(true);
         var maybeNull = Maybe.of(null);
         expect(maybeNull instanceof Nothing).to.equal(true);
     });
 
-    it("'value' getter returns correct value", () => {
+    it("'value' getter returns value from monad or throws error", () => {
         var maybe1 = Maybe.of("value");
         expect(maybe1.value).to.equal("value");
         var maybe2 = Maybe.of(null);
         assert.throws(() => { maybe2.value }, TypeError, "No value in Nothing");
     });
 
-    it("'map' method returns the correct object", () => {
+    it("'map' method applies the given function on monad value and returns wrapped result", () => {
         var maybe1 = Maybe.of(1);
         var func = (val) => val + 1;
         var res1 = maybe1.map(func);
@@ -40,7 +40,7 @@ describe("testing Maybe monad", () => {
         assert.throws(() => { maybe2.value }, TypeError, "No value in Nothing");
     });
 
-    it("'chain' method returns the correct object", () => {
+    it("'chain' method applies the given function on monad value and returns result", () => {
         var maybe1 = Maybe.of(1);
         var func = (val) => val + 1;
         var res1 = maybe1.chain(func);
@@ -52,7 +52,7 @@ describe("testing Maybe monad", () => {
         assert.throws(() => { maybe2.value }, TypeError, "No value in Nothing");
     });
 
-    it("'getOrElse' method returns the correct object", () => {
+    it("'getOrElse' method returns monad value or alternative object depending on monad type", () => {
         var maybe1 = Maybe.of(1);
         var res1 = maybe1.getOrElse(100);
         expect(res1 instanceof Maybe).to.equal(false);
@@ -63,7 +63,7 @@ describe("testing Maybe monad", () => {
         expect(res2).to.equal(100);
     });
 
-    it("'isJust' getter returns boolean indicating if object is of Just type", () => {
+    it("'isJust' getter returns boolean indicating if monad is of Just type", () => {
         var maybe1 = Maybe.of(1);
         expect(maybe1 instanceof Just).to.equal(true);
         expect(maybe1.isJust).to.equal(true);
@@ -72,7 +72,7 @@ describe("testing Maybe monad", () => {
         expect(maybe2.isJust).to.equal(false);
     });
 
-    it("'isNothing' getter returns boolean indicating if object is of Nothing type", () => {
+    it("'isNothing' getter returns boolean indicating if monad is of Nothing type", () => {
         var maybe1 = Maybe.of(1);
         expect(maybe1 instanceof Nothing).to.equal(false);
         expect(maybe1.isNothing).to.equal(false);
