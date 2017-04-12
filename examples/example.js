a12aa315095d1e05fb4e4bf6bdc61d5ff898ee4a
Milos Stojanovic <stojanovicmilos92@outlook.com>
2017-04-12 22:28:27 +0200

    Written an example for maybe monad

--- examples/example.js
+++ examples/example.js
@@ -1,13 +1,9 @@
 var Funky = require("./../dist/funky.min");
 var Student = require("./student");
 
-var gradesGetter = function (student) {
+var getGrades = function (student) {
     return Funky.read(Funky.prop("grades"), student);
 };
-
-function getGrades(student) {
-    return gradesGetter(student);
-}
 
 function calculateAverage(arr) {
     var sum = 0;
@@ -22,8 +18,22 @@ function calculateAverageGrade(student) {
         .of(student)
         .map(getGrades)
         .map(calculateAverage)
-        .getOrElse("Null value provided");
+        .getOrElse("Cannot calculate average grade, null value provided");
 }
 
-console.log(calculateAverageGrade(Student));
-console.log(calculateAverageGrade(null));
+function demoMaybeMonad() {
+    console.log("Maybe monad provides null tolerant processing pipeline. Null checking is provided out of the box.\n");
+    console.log("\nCalculate average grade for a given student: ");
+    console.log("\tName: " + Student.name);
+    var grades = "";
+    Student.grades.forEach(function (e) {
+        grades += " " + e;
+    });
+    console.log("\tGrades: " + grades);
+    console.log("\tAverage grade: " + calculateAverageGrade(Student));
+    console.log("\nCalculate average grade for a null student: ");
+    console.log("\t" + calculateAverageGrade(null));
+    console.log("--------------------------------------------------------------------------------------------------------");
+}
+
+demoMaybeMonad();
